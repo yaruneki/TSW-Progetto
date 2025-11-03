563fed55e66e1b7f839bfe4d004bc35d96e78874
Antonio <35410603+redyz13@users.noreply.github.com>
2025-11-03 21:15:04 +0100

    build: clean migration to Maven and finalize project structure

--- WebContent/js/Modal.js
+++
@@ -1,8 +0,0 @@
-//Open and close Modal Window
-function Modal() {
-	if (document.getElementById("myModal").style.visibility == "hidden"){
-		document.getElementById("myModal").style.visibility = "visible";
-	}else{
-		document.getElementById("myModal").style.visibility = "hidden";
-	}
-}
